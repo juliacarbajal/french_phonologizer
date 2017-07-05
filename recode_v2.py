edfd708e912271dc0bd2a8d3d33efb5f7ff2d8d0
Julia C <carbajal.mjulia@gmail.com>
2017-07-05 19:41:54 +0200

    Added more mandatory cases (dans, chez, en, sans).

--- recode_v2.py
+++ recode_v2.py
@@ -1,5 +1,5 @@
 # This Python file uses the following encoding: utf-8
-import sys
+import sys # This is not necessary anymore (but I'll keep it as a reminder that I might use it)
 
 f = open('liaisonv2.txt', 'w')
 foutput = open('outputpruebav2.txt','w')
@@ -49,21 +49,21 @@ adjectives = []
 with open('output_ADJ.txt') as ADJlist:
 	for line in ADJlist:
 		line = line.strip()
-		if line[-1] in liaison:
+		if line[-1] in liaison: # Only adjectives finishing in a liaison consonant
 			adjectives.append(line)
 	
 liaison_words = ['un', 'des', 'les', 'ces',\
                  'mon', 'ton', 'son', 'mes', 'tes', 'ses', 'nos', 'vos', 'leurs',\
 				 'aux', 'aucun', 'tout', 'quels', 'quelles', 'quelques',\
 				 'on', 'nous', 'vous', 'ils', 'elles',\
-				 'est', 'ont',\
+				 'est', 'ont', 'chez', 'dans', 'en', 'sans',\
 				 'plus','très','bien','quand','comment','trop','beaucoup']
 
 liaison_words = liaison_words + adjectives
-exceptions_next = ['et','oh','euh','hein', 'ah']
+exceptions_next = ['et','oh','euh','hein', 'ah', 'ou']
 
 # Read line by line and search words in dictionary:
-count_liaison = 0
+#count_liaison = 0
 with open('extract.txt') as input_file:
 	for j, line in enumerate(input_file):
 		line = line.decode('cp1252').encode('utf-8')
@@ -81,7 +81,7 @@ with open('extract.txt') as input_file:
 					if firstphon in vowels and \
 					nextword not in exceptions_next:
 						newwords[i] += liaison[lastletter] # Attach liaison consonant
-						count_liaison += 1
+						#count_liaison += 1
 						unedited = (word + ' ' + nextword).decode('utf-8').encode('cp1252').ljust(30) # Reencode in ANSI to left-justify
 						unedited = unedited.decode('cp1252').encode('utf-8') # Back to unicode for printing
 						edited = (newwords[i] + ' ' + dico[nextword])
