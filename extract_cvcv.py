f635e02aa7da79f9cc4881f9f72176b668b31e3a
Julia C <carbajal.mjulia@gmail.com>
2017-08-01 11:30:01 +0200

    Improved printing list. Changed output file name.

--- extract_cvcv.py
+++ extract_cvcv.py
@@ -2,19 +2,20 @@
 import re
 from collections import Counter
 
-f = open('CVCV.txt', 'w')
+f = open('disyllables.txt', 'w')
 
+# Define phonemes:
 obstruents = ['p', 'b', 't', 'd', 'k', 'g', 'f', 'v', 's', 'z', 'S', 'Z']
 liquids = ['l', 'R']
 nasals = ['m', 'n', 'N']
 foreign = ['x', 'G']
+
 consonants = obstruents + liquids + nasals + foreign
-
 vowels = ['a','i','e','E','o','O','u','y','§','1','5','2','9','@','°','3']
-
 semivowels = ['j','8','w']
 
 def syllabic_structure(syl):
+	# This functions extracts the syllabic structure of any syllable (e.g. CV, CVC, etc)
 	structure = ''
 	for phone in syl:
 		if phone in consonants:
@@ -27,6 +28,13 @@ def syllabic_structure(syl):
 			structure += 'O'
 	return structure
 
+def print_output(syllable1, syllable2, counts, structure1, structure2, file):
+	structure = (str(structure1) + ' ' + str(structure2)).ljust(15)
+	syllables = (str(syllable1) + ' ' + str(syllable2)).decode('utf-8').encode('cp1252').ljust(15)
+	syllables = syllables.decode('cp1252').encode('utf-8')
+	print >> file, structure + syllables + str(counts)
+
+# First: retrieve all disyllables
 syllabic_dict = {}
 with open('recoded_L_D_S_E.txt') as corpus:
 	for line in corpus:
@@ -39,15 +47,14 @@ with open('recoded_L_D_S_E.txt') as corpus:
 			else:
 				syllabic_dict[syllable] = syllabic_dict[syllable] + [text[i+1]]
 
-for key in syllabic_dict:
-	following_syllables = syllabic_dict[key]
+# Second: extract syllabic structure and count
+for S1 in syllabic_dict:
+	following_syllables = syllabic_dict[S1]
 	syl_counts = Counter(following_syllables)
-	S1_structure = syllabic_structure(key.decode('utf-8').encode('cp1252'))
-	for syllable in list(set(following_syllables)):
-		N = syl_counts[syllable]
-		# Here add line to translate the syllable into CV, CYV etc coding.
-		S2_structure = syllabic_structure(syllable.decode('utf-8').encode('cp1252'))
-		to_print = [S1_structure, S2_structure, str(key), str(syllable), str(N)]
-		print >> f, '\t'.join(to_print)
+	S1_structure = syllabic_structure(S1.decode('utf-8').encode('cp1252'))
+	for S2 in list(set(following_syllables)):
+		N = syl_counts[S2]
+		S2_structure = syllabic_structure(S2.decode('utf-8').encode('cp1252'))
+		print_output(S1, S2, N, S1_structure, S2_structure, f)
 
 f.close()
